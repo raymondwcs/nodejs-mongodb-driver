b048143fd3f13e0526f2951acfab9f3cdf134697
Raymond SO <rso@ouhk.edu.hk>
2015-10-14 09:03:55 +0800

    filter example

---
+++ filter/server.js
@@ -0,0 +1,25 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/test';
+
+var aggregateRestaurants = function(db, callback) {
+	var cursor = db.collection('restaurants').aggregate(
+	[
+		{$match: {"borough": "Queens", "cuisine": "Brazilian"}},
+		{$group: {"_id": "$address.zipcode", "count": {$sum: 1}}}
+	]
+	).toArray(function(err, result) {
+		assert.equal(err,null);
+		console.log(result);
+		callback(result);
+	});
+};
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  aggregateRestaurants(db, function() {
+      db.close();
+  });
+});
+
