0d76ae4e4bc3726d937058b2c0712b44d0e6885e
Raymond SO <rso@ouhk.edu.hk>
2015-10-14 08:30:18 +0800

    insert multiple documents

---
+++ insertN/server.js
@@ -0,0 +1,26 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/test';
+
+
+var books = [
+{"name": "Intro to Node.js","author": "John Dole", "price": 75.0, "stock":0},
+{"name": "Intro to MongoDB","author": "John Smith", "price": 85.0, "stock":10}
+];
+
+var insertDocument = function(db, callback) {
+	db.collection('books').insert(books, function(err, result) {
+		assert.equal(err, null);
+		console.log("Inserted a document into the books collection.");
+		callback(result);
+	});
+};
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  insertDocument(db, function() {
+      db.close();
+  });
+});
+
