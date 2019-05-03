fd26a6413b5bc258b3d92febf01f86f7005404aa
Raymond SO <rso@ouhk.edu.hk>
2019-05-03 08:22:13 +0800

    initial version

---
+++ connect/README.md
@@ -0,0 +1,12 @@
+# Node.js Example: Connect to MongoDB at mlab
+This example shows you how to connect mlab
+## Getting Started
+Change the value of variable `url` in `server.js` to the address of your [mlab server](http://mlab.com).
+### Installing
+```
+npm install
+```
+### Running
+```
+npm start
+```
---
+++ connect/server.js
@@ -0,0 +1,16 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const ObjectId = require('mongodb').ObjectID;
+const url = '';
+const dbName = 'comps381f';
+
+const client = new MongoClient(url);
+
+client.connect(function(err) {
+   assert.equal(null,err);
+   console.log("Connected successfully to server");
+
+   const db = client.db(dbName);
+
+   client.close();
+});
