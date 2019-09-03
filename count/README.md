b22226743175c8a68aa1bf85413f30e61cdcab18
Raymond SO <rso@ouhk.edu.hk>
2019-09-03 14:32:09 +0800

    initial version

---
+++ count/README.md
@@ -0,0 +1,15 @@
+# Node.js Example: Reading MongoDB documents
+An example of counting the number of documents in the `restaurant` collection
+
+## Getting Started
+1. Change the value of variable `url` in `server.js` to the address of your [mlab server](http://mlab.com).
+2. You need a collection named `restaurant` with a few documents in it.
+
+### Installing
+```
+npm install
+```
+### Running
+```
+npm start
+```
---
+++ count/server.js
@@ -0,0 +1,28 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const ObjectId = require('mongodb').ObjectID;
+const url = '';  // mlab url
+const dbName = ''; // mlab db
+
+
+const countRestaurants = function(db, callback) {
+	var collection = db.collection('restaurant');
+	collection.countDocuments(function(err,count) {
+		assert(null,err);
+		console.log(`There are ${count} documents in the restuarant collection`);
+	})
+	callback();
+}
+
+const client = new MongoClient(url);
+
+client.connect(function(err) {
+   assert.equal(null,err);
+   console.log("Connected successfully to server");
+
+   const db = client.db(dbName);
+
+	countRestaurants(db, function() {
+		client.close();
+	})
+})
