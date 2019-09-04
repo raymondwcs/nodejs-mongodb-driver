b76b6482f4d3d130cf34fa7781b7c34c05ce9d8f
Raymond So <rso@Raymonds-MacBook-Pro.local>
2019-09-04 13:24:24 +0800

    fixed minor bugs

--- count/server.js
+++ count/server.js
@@ -4,18 +4,16 @@ const ObjectId = require('mongodb').ObjectID;
 const url = '';  // mlab url
 const dbName = ''; // mlab db
 
-
 const countRestaurants = function (db, callback) {
 	var collection = db.collection('restaurant');
-	collection.countDocuments(function (err, count) {
-		assert(null, err);
+	collection.countDocuments({},{},function (err, count) {
+		assert.equal(null, err);
 		console.log(`There are ${count} documents in the restuarant collection`);
 	})
 	callback();
 }
 
 const client = new MongoClient(url);
-
 client.connect(function (err) {
 	assert.equal(null, err);
 	console.log("Connected successfully to server");
