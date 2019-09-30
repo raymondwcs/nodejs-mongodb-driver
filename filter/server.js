50c94507bc2260034c959f67314d2ce2b665ac5a
Raymond SO <rso@ouhk.edu.hk>
2019-09-30 22:06:44 +0800

    find which zipcode in Queens has the most number of Brazilian restaurants

--- filter/server.js
+++ filter/server.js
@@ -1,8 +1,8 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const ObjectId = require('mongodb').ObjectID;
-const url = '';
-const dbName = '';
+const url = 'mongodb+srv://student:comps381f@cluster0-xljee.mongodb.net/test?retryWrites=true&w=majority';
+const dbName = 'test';
 
 const aggregateRestaurants = (db, callback) => {
 	var cursor = db.collection('restaurant').aggregate(
@@ -13,6 +13,18 @@ const aggregateRestaurants = (db, callback) => {
 	).toArray((err, result) => {
 		assert.equal(err,null);
 		console.log(result);
+		/*
+		* Lets find which zipcode in Queens has the most number of Brazilian restaurants
+		*/
+		let max = 0;
+		let maxZip = '';
+		result.forEach((x) => {
+			if (x.count > max) {
+				max = x.count;
+				maxZip = x._id;	
+			}
+		})
+		console.log(`Zipcode ${maxZip} in Queens has the most number of Brazilian restaurants`);
 		callback(result);
 	});
 };
