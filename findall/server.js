fffd1f3f7f4dd5cabefdd915b14d3e465694361f
Raymond So <rso@ouhk.edu.hk>
2019-09-30 14:39:18 +0800

    used more ES6 syntax

--- findall/server.js
+++ findall/server.js
@@ -4,26 +4,22 @@ const ObjectId = require('mongodb').ObjectID;
 const url = '';
 const dbName = '';
 
-const findRestaurants = function(db, callback) {
-   var cursor = db.collection('restaurant').find();
-   cursor.each(function(err, doc) {
-      assert.equal(err, null);
-      if (doc != null) {
-         console.dir(doc);
-      } else {
-         callback();
-      }
+const findRestaurants = (db, callback) => {
+   let cursor = db.collection('restaurant').find().limit(10)
+   cursor.forEach((doc) => {
+      console.log(JSON.stringify(doc));
    });
+   callback();
 };
 
 const client = new MongoClient(url);
-client.connect(function(err) {
+client.connect((err) => {
    assert.equal(null,err);
    console.log("Connected successfully to server");
 
    const db = client.db(dbName);
  
-   findRestaurants(db,function(){
+   findRestaurants(db,() => {
       client.close();
    })
 })
