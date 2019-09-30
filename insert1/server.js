7a1fcad7e67575592c34a1e2f616b2c53de9c371
Raymond So <rso@ouhk.edu.hk>
2019-09-30 14:13:50 +0800

    used more ES6 syntax

--- insert1/server.js
+++ insert1/server.js
@@ -4,26 +4,26 @@ const ObjectId = require('mongodb').ObjectID;
 const url = '';
 const dbName = '';
 
-const insertDocument = function(db, callback) {
+const insertDocument = (db, callback) => {
    db.collection('books').insertOne( {
-	"name" : "Introduction to Node.js",
-	"author" : "John Dole",
-	"price" : 75.00,
-	"stock" : 0      
-   }, function(err, result) {
-    assert.equal(err, null);
-    console.log("Inserted a document into the books collection.");
-    callback(result);
+    "name" : "Introduction to Node.js",
+    "author" : "John Dole",
+    "price" : 75.00,
+    "stock" : 0      
+   }, (err, result) => {
+      assert.equal(err, null);
+      console.log("Inserted one document into the books collection.");
+      callback(result);
   });
 };
 
 const client = new MongoClient(url);
-client.connect(function(err) {
+client.connect((err) => {
   assert.equal(null,err);
   console.log("Connected successfully to server");
 
   const db = client.db(dbName);
-  insertDocument(db, function() {
+  insertDocument(db, () => {
       client.close();
   });
 });
